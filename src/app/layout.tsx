da68ff33bfb4e73d777932b9c13aa56f35c4ebb5
William Phan <willdphan@gmail.com>
2024-08-29 15:19:02 -0700

    room

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Montserrat, Montserrat_Alternates } from 'next/font/google';
 
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/utils/cn';
-import { Room } from '@/app/Room';
+import { Room } from './Room.tsx';
 import '@/styles/globals.css';
 
 const Analytics = dynamic(() => 
