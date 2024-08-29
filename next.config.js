609c5234f5e5d3e2ff6533a847022cfa02155318
William Phan <willdphan@gmail.com>
2024-08-29 15:39:36 -0700

    room

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-
-module.exports = nextConfig
 const nextConfig = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'http://127.0.0.1:8000/api/:path*', // Make sure this is http
-  //     },
-  //   ]
-  // },
+  // other configurations...
 }
 
-export default nextConfig;
+module.exports = nextConfig
